68b89e5db9a09d9d9d28096f200bd603d1989eee
Nikhil Sharma <nikhilsharma26500@outlook.com>
2024-09-08 17:52:06 -0400

    added routes for food

--- cosmetics/controller/conversation.py
+++ cosmetics/controller/conversation.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter, HTTPException
+from db_functions import *
+from database import table_creation_session
 
 router = APIRouter(prefix="/cosmetics", tags=["Cosmetics"])
+
