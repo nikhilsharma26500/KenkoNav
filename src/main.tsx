7b499f621586c84ad07d9fcd3f5e657626144da3
Nikhil Sharma <nikhilsharma26500@outlook.com>
2024-09-30 23:34:46 -0400

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
+import App from './App.tsx'
 import Food from './food/food.tsx'
 import Cosmetics from './cosmetics/cosmetics.tsx'
 import Waitlist from './waitlist/waitlist_temp.tsx'
 import './index.css'
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  // Navigate
+} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-      <Route path="/" element={<Navigate to="/waitlist"/>}/>
-      {/* <Route path="/food" element={<Food/>}/>
-      <Route path="/cosmetics" element={<Cosmetics/>}/> */}
-      <Route path="/demo/food" element={<Food/>}/>
-      <Route path="/demo/cosmetics" element={<Cosmetics/>}/>
-      <Route path="/waitlist" element={<Waitlist/>}/>
+        {/* <Route path="/" element={<Navigate to="/waitlist"/>}/> */}
+        <Route path='/' element={<App />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/cosmetics" element={<Cosmetics />} />
+        <Route path="/demo/food" element={<Food />} />
+        <Route path="/demo/cosmetics" element={<Cosmetics />} />
+        <Route path="/waitlist" element={<Waitlist />} />
       </Routes>
     </Router>
   </StrictMode>,
